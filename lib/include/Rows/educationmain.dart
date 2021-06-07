755f823298c72279b91de85277a8b9225fc09192
Riiquezz <henriquepomatti@hotmail.com>
2021-06-06 23:50:22 -0300

    Necessário revisar tablet e mobile

--- lib/include/Rows/educationmain.dart
+++ lib/include/Rows/educationmain.dart
@@ -69,6 +69,61 @@ class EduDesk extends StatelessWidget {
               )
             ],
           ),
+          SizedBox(
+            width: 25,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 15,
+              ),
+              Container(
+                height: 150,
+                width: 150,
+                alignment: Alignment.centerLeft,
+                decoration: BoxDecoration(
+                  boxShadow: [
+                    new BoxShadow(
+                      color: Colors.black.withOpacity(0.1),
+                      blurRadius: 15.0,
+                      spreadRadius: 5.0,
+                      offset: Offset(
+                        0.0, // Move to right 10  horizontally
+                        10.0, // Move to bottom 10 Vertically
+                      ),
+                    ),
+                  ],
+                  shape: BoxShape.rectangle,
+                  image: DecorationImage(
+                    fit: BoxFit.contain,
+                    image: AssetImage('assets/images/wizard.png'),
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Wizard - By Pearson",
+                    style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "English teaching",
+                    style: TextStyle(fontSize: 26, fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    "2015 - 2017",
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
         ],
       ),
     );
